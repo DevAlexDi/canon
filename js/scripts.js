4b1ad3dc3f7adefb89855c29bd2bcf06ec4d346e
keitmark <keitmark@mail.ru>
2019-01-18 22:39:39 +0300

    save

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
    
-
+    $("#to_top").click(function(){
+        $("html, body").animate({scrollTop:0},500);
+    });
+    
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 0){
+            $('#to_top').addClass('showed');
+        }
+        else{
+            $('#to_top').removeClass('showed');
+        }
+    });
     
 
 });
