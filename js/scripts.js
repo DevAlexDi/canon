2b7866baf151d1483a56aaf740f44ca61a08324b
unknown <keitmark@mail.ru>
2019-01-21 18:14:32 +0300

    home page done

--- js/scripts.js
+++ js/scripts.js
@@ -13,5 +13,32 @@ $(document).ready(function(){
         }
     });
     
+    $('.menu__list__item--sub-menu').click(function(e){
+        e.stopPropagation();
+        if(!$(this).hasClass('opened')){
+            $(this).addClass('opened');
+        }
+        else{
+            $(this).removeClass('opened');
+        }
+        
+    });
+    $('body').click(function(){
+        if($('.menu__list__item--sub-menu').hasClass('opened')){
+            $('.menu__list__item--sub-menu').removeClass('opened');
+        }
+    });
+    
+    $('.see-more').click(function(){
+        $('.price__left__list--hidden').addClass('showed');
+        $(this).addClass('hide');
+    });
 
+    
+     $('.tel-inp').inputmask({
+        "mask": "+7 (999) 999-99-99"
+        , "placeholder": "_"
+        , showMaskOnHover: false
+        , showMaskOnFocus: true
+    });
 });
